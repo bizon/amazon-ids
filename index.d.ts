77922e13cc4e358941ce0806bdcd9444832fa709
Mesquita Vincent <vincent.mesquita@bizon.solutions>
2021-06-28 14:33:29 +0200

    Fix types (#65)

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare interface Marketplace {
+export declare interface Marketplace {
   code: string;
   id: string;
   name: string;
@@ -11,36 +11,41 @@ declare interface Marketplace {
 }
 
 /**
+ * All the available marketplaces
+ */
+export declare const marketplaces: Marketplace[]
+
+/**
  * Get a marketplace by its id
  * @param id  The marketplace's id
  * @returns The marketplace
  */
-declare function getMarketplaceById(id: string): Marketplace | undefined
+export declare function getMarketplaceById(id: string): Marketplace | undefined
 
 /**
  * Get a marketplace by its code
  * @param code The marketplace's code
  * @returns The markerplace
  */
-declare function getMarketplaceByCode(code: string): Marketplace | undefined
+export declare function getMarketplaceByCode(code: string): Marketplace | undefined
 
 /**
  * Get a marketplace by its domain
  * @param domain The marketplace's domain
  * @returns The marketplace
  */
-declare function getMarketplaceByDomain(domain: string): Marketplace | undefined
+export declare function getMarketplaceByDomain(domain: string): Marketplace | undefined
 
 /**
  * Get several marketplaces by a MWS domain
  * @param domain The MWS domain
  * @returns The marketplaces
  */
-declare function getMarketplacesByMwsDomain(domain: string): Marketplace[]
+export declare function getMarketplacesByMwsDomain(domain: string): Marketplace[]
 
 /**
  * Get several marketplaces by a MWS region
  * @param mwsRegion The MWS region
  * @returns The marketplaces
  */
-declare function getMarketplacesByMwsRegion(mwsRegion: string): Marketplace[]
+export declare function getMarketplacesByMwsRegion(mwsRegion: string): Marketplace[]
